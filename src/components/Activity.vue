d51833cf91d6f867debaf9355f5164ec5dc74db1
Alexa Noel <alexanowel@live.fr>
2021-03-07 23:21:46 +0100

    activity detail tile and activity page style

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -1,29 +1,33 @@
 <template>
   <div v-if="dopactivity.summary" class="activity">
-    <a v-on:click="goBackToSos"> go back pls </a>
     <div>
       <h1> {{dopactivity.summary}} </h1>
     </div>
     <div class="activity-actions">
-      <div class="activity-tile" id="activity-reward-info">
-        <p>TODO iterate over all rewards</p>ßü´
-      </div>
-      <div class="activity-tile" id="activity-done-button">
-        <p> Did it ! </p>
-      </div>
-      <div class="activity-tile" id="activity-cantdo-button">
-        <p>Can't do..</p>
-      </div>
+      <ActivityDetail icon="clock.svg" amount="5"/>
+      <ActivityDetail icon="chore.svg" amount="10"/>
+      <ActivityDetail icon="heart.svg" amount="0"/>
+      <ActivityDetail icon="home.svg" amount="0"/>
+    </div>
+    <div class="activity-followup">
+      <Button class="smallButton" id="nope" smallText="Can't do" v-on:clicked="goBackToSos" />
+      <Button class="smallButton" id="done" smallText="Done" v-on:clicked="goBackToSos" />
     </div>
   </div>
 </template>
 
 <script>
+import ActivityDetail from '@/components/atoms/ActivityDetail.vue'
+import Button from '@/components/atoms/Button.vue'
 export default {
   name: 'SOS',
   props: {
     msg: String,
     dopactivity: Object
+  },
+  components: {
+    ActivityDetail,
+    Button
   },
   methods: {
     goBackToSos: function () {
@@ -35,6 +39,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Beth+Ellen&display=swap');
+
+h1 {
+  font-family: Beth Ellen;
+  color: #FDF7FF;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -51,7 +61,27 @@ a {
 }
 
 .activity {
-  background: red;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(to right top,
+    #4c0bb7,
+    #7100b0,
+    #8b00a9,
+    #a000a3,
+    #b2009c,
+    #d2008e,
+    #ea1e7f,
+    #fc3c70,
+    #ff6e59,
+    #ff9f4b,
+    #ffcd51,
+    #f9f871);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 }
 
 .activity-actions {
@@ -59,5 +89,28 @@ a {
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
+}
+
+.activity-followup {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 64px;
+}
+
+.smallButton {
+  width: 150px;
+  height: 150px;
+  color: #FDF7FF;
+  margin: 16px;
+}
+
+#nope {
+  background: #FF066A;
+}
+
+#done {
+  background: #479F90;
 }
 </style>
---
+++ src/components/atoms/ActivityDetail.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="tile">
+    <div class="icon">
+        <img :src="require(`@/assets/icons/${icon}`)" />
+    </div>
+    <div class="amount-tile">
+        <p class=""> {{amount}} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ActivityDetail',
+  props: {
+    amount: String,
+    icon: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.tile {
+    background: #FDF7FF;
+    border-radius: 25px;
+    width: 150px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 16px;
+}
+img {
+    color: #FFBA43;
+    width: 50px;
+    height: 50px;
+}
+
+.icon {
+    height: 100px;
+    width: 150px;
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: #FDF7FF;
+}
+
+.amount-tile {
+    background: #FFBA43;
+    border-radius: 0px 0px 25px 25px;
+    width: 150px;
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    color: #FDF7FF;
+    font-size: 52px;
+}
+
+</style>
